e53aa72c61a67daeff888dd7675c8e7f236896b2
Stella Dare <stelladare2@gmail.com>
2018-12-10 15:34:57 +0000

    side bar added

--- js/script.js
+++ js/script.js
@@ -36,3 +36,20 @@ window.onclick = function(event) {
     }
 }
 /************************Modal JS End************************************************/
+
+/***************************************Side bar Nav*******************************/
+//Getting the menu bar
+let menuBar = document.getElementById('menu-bar');
+//Getting sidebar
+let sideBar = document.getElementById('mySidenav');
+//Get sidebar close character
+let sideClose = document.getElementById('sideclosebtn')
+//Function for showing side bar
+let showSideBar =() => sideBar.style.width = "250px";
+//Function to close side bar
+let closeSideBar =() => sideBar.style.width = "0%";
+//Event listener for side bars
+menuBar.addEventListener('click',showSideBar);
+sideClose.addEventListener('click',closeSideBar);
+
+/*******************************Side bar Nav End*************************************/
