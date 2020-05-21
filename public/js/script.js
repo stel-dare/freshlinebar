4aaa029d9f2872d7985dbdbe84f3fb6d5c86c8ca
stelladare2@gmail.com <stelladare2@gmail.com>
2020-05-21 23:24:53 +0000

    Create Responsive Nav Bar

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+function dropdown(){
+    document.getElementById("mobile-nav-links").classList.toggle("hide");
+    document.getElementById("menu-span").classList.toggle("focus");
+    document.getElementById("nav-overlay").classList.toggle("nav-overlay");
+}
