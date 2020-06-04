5f2ba3ad7ab1e1aa6fea15825b6bdc9f4af97aed
stelladare2@gmail.com <stelladare2@gmail.com>
2020-06-04 22:15:26 +0000

    Animate menu item details to grab user attention

--- public/js/script.js
+++ public/js/script.js
@@ -34,12 +34,23 @@ function showSlide(n){
 }
 
 //Function for displaying menu details
+// function displayDetails(dish){
+// let allDishes = document.getElementsByClassName('meal-item-details');
+// let hidden = dish.getElementsByClassName('meal-item-details')[0].classList.contains('hide');
+// for(let i=0; i<allDishes.length;i++){allDishes[i].classList.add('hide');}
+// hidden? dish.getElementsByClassName('meal-item-details')[0].classList.remove('hide') : dish.getElementsByClassName('meal-item-details')[0].classList.add('hide');
+// }
+
+//Function for displaying menu details through opacity and stoping animation
 function displayDetails(dish){
-let allDishes = document.getElementsByClassName('meal-item-details');
-let hidden = dish.getElementsByClassName('meal-item-details')[0].classList.contains('hide');
-for(let i=0; i<allDishes.length;i++){allDishes[i].classList.add('hide');}
-hidden? dish.getElementsByClassName('meal-item-details')[0].classList.remove('hide') : dish.getElementsByClassName('meal-item-details')[0].classList.add('hide');
-}
+    let allDishes = document.getElementsByClassName('meal-item-details');
+    let dishOpacity = dish.getElementsByClassName('meal-item-details')[0].classList.contains('opacity');
+    for(let i=0; i<allDishes.length;i++){allDishes[i].classList.remove('fadeX');}
+    for(let i=0; i<allDishes.length;i++){allDishes[i].classList.add('opacity');}
+    // console.log(dishOpacity); 
+    dishOpacity? dish.getElementsByClassName('meal-item-details')[0].classList.remove('opacity') : dish.getElementsByClassName('meal-item-details')[0].classList.add('opacity');
+    }
+
 
 //Google maps marker function
 function initMap() {
