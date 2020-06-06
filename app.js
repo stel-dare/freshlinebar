7e6d4996c1e09a7cfc3ee4495ce4396331be1e0e
stelladare2@gmail.com <stelladare2@gmail.com>
2020-06-06 21:08:02 +0000

    Create Responsive Contact-Us page

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const express = require('express');
 app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/index.html');
+    res.render('index');
 });
+
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+})
+
 app.listen(process.env.PORT || 3000,()=>{
     console.log('Listening on port 3000');
 });
