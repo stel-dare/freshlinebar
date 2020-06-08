30af99aae885e91eb4acd3d9aaf0e4f077f7cc0a
stelladare2@gmail.com <stelladare2@gmail.com>
2020-06-08 19:16:52 +0000

    Create rresponsive services page

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.get('/contact',(req,res)=>{
 app.get('/about',(req,res)=>{
     res.render('about');
 });
+app.get('/services',(req,res)=>{
+    res.render('services');
+});
 app.listen(process.env.PORT || 3000,()=>{
     console.log('Listening on port 3000');
 });
